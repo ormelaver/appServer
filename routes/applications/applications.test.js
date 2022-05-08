1e52d604bd7a4a85807b4b11fea4c4fecb4c4a69
Or Melaver <ormelaver@gmail.com>
2022-05-08 19:47:30 +0300

    trying to fix tests fails in GH-actions

--- routes/applications/applications.test.js
+++ routes/applications/applications.test.js
@@ -28,7 +28,7 @@ describe('Test /applications endpoint', () => {
         // POPULATE THE DB BEFORE TESTING IN GITHUB ACTIONS
         test('Should respond with 200 success', async () => {
             const response = await request(app)
-                .get('/applications/relevantApplication?age=30&category=social&customertype=bronze')
+                .get('/applications/relevantapplication?age=30&category=social&customertype=bronze')
                 .expect('Content-Type', /json/)
                 .expect(200)
                 .expect(verifyCategory);
@@ -36,7 +36,7 @@ describe('Test /applications endpoint', () => {
 
         test('Should respond with 400, missing parameter', async () => {
             const response = await request(app)
-                .get('/applications/relevantApplication?category=social&customertype=bronze')
+                .get('/applications/relevantapplication?category=social&customertype=bronze')
                 .expect('Content-Type', /json/)
                 .expect(400);
 
@@ -53,7 +53,7 @@ describe('Test /applications endpoint', () => {
 
         test('Should respond with 404 not found', async () => {
             const response = await request(app)
-                .get('/applications/relevantApplication?age=30&category=social&customertype=blue')
+                .get('/applications/relevantapplication?age=30&category=social&customertype=blue')
                 .expect('Content-Type', /json/)
                 .expect(404);
 
@@ -64,7 +64,7 @@ describe('Test /applications endpoint', () => {
 
         test('Should respond with 400, invalid app', async () => {
             const response = await request(app)
-                .get('/applications/relevantApplication?age=30&category=socal&customertype=bronze')
+                .get('/applications/relevantapplication?age=30&category=socal&customertype=bronze')
                 .expect('Content-Type', /json/)
                 .expect(400);
 
@@ -104,7 +104,7 @@ describe('Test /applications endpoint', () => {
         }
         test('Should respond with 201, success: true', async () => {
             const response = await request(app)
-                .post('/applications/installedApps')
+                .post('/applications/installedapps')
                 .send(completeAppData)
                 .expect('Content-Type', /json/)
                 .expect(201);
@@ -114,7 +114,7 @@ describe('Test /applications endpoint', () => {
 
         test('Should respond with 400, missing parameter', async () => {
             const response = await request(app)
-                .post('/applications/installedApps')
+                .post('/applications/installedapps')
                 .send(appDataAgeTypo)
                 .expect('Content-Type', /json/)
                 .expect(400);
@@ -126,7 +126,7 @@ describe('Test /applications endpoint', () => {
 
         test('Should respond with 400, age parameter is not an Int', async () => {
             const response = await request(app)
-                .post('/applications/installedApps')
+                .post('/applications/installedapps')
                 .send(appDataAgeNotInt)
                 .expect('Content-Type', /json/)
                 .expect(400);
@@ -138,7 +138,7 @@ describe('Test /applications endpoint', () => {
 
         test('Should respond with 404, app not found', async () => {
             const response = await request(app)
-                .post('/applications/installedApps')
+                .post('/applications/installedapps')
                 .send(appDataWrongName)
                 .expect('Content-Type', /json/)
                 .expect(404);
